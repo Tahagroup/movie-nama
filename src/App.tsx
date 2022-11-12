379f3ce46852153e9061b0068e268815cd4c3581
Archer <y.basiri@yahoo.com>
2022-11-12 20:31:03 +0330

    https to https

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import useFetch from "./hooks/useFetch";
 function App() {
   const [searchText, setSearchText] = useState<string>("batman");
 
-  const URL_TO_FETCH = `http://www.omdbapi.com/?s=${searchText}&apikey=62f005b3`;
+  const URL_TO_FETCH = `https://www.omdbapi.com/?s=${searchText}&apikey=62f005b3`;
   const [data, error, loading] = useFetch(
     // "http://www.omdbapi.com/?i=tt3896198&apikey=wrong" // a single movie
     URL_TO_FETCH
